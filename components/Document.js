99d02ef42224314542a3c92940d12b5e3fa710fe
Stephan <stephangdenis@gmail.com>
2021-07-12 13:04:48 -0300

    Component[Document]: Includes[DocumentCreate][DocumentHistory]

---
+++ components/Document.js
@@ -0,0 +1,13 @@
+import DocumentCreate from "./DocumentCreate";
+import DocumentHistory from "./DocumentHistory";
+
+const Document = () => {
+  return (
+    <>
+      <DocumentCreate />
+      <DocumentHistory />
+    </>
+  );
+};
+
+export default Document;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Document from "../components/Document";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Document />
     </div>
   );
 }
