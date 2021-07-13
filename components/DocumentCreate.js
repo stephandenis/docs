b6b3fcf1b1e998b83f55506f1d5c31a9535caf4e
Stephan <stephangdenis@gmail.com>
2021-07-13 16:37:28 -0300

    Modal to popup when creating a new document

--- components/DocumentCreate.js
+++ components/DocumentCreate.js
@@ -1,9 +1,46 @@
 import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
 import Image from "next/image";
+import Modal from "@material-tailwind/react/Modal";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
+import { useState } from "react";
 const DocumentCreate = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
+  const createDocument = () => {};
+
+  const modal = (
+    <Modal size="sm" active={showModal} toggler={() => setShowModal(false)}>
+      <ModalBody>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="outline-none w-full"
+          placeholder="Enter name of document..."
+          onKeyDown={(e) => e.key === "Enter" && createDocument()}
+        />
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          color="blue"
+          buttonType="link"
+          onClick={(e) => setShowModal(false)}
+          ripple="dark"
+        >
+          Cancel
+        </Button>
+
+        <Button color="blue" onClick={createDocument} ripple="light">
+          Create
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
   return (
     <section className="bg-[#F8F9FA] pb-10 px-10">
+      {modal}
       <div className="max-w-3xl mx-auto">
         <div className="flex items-center justify-between py-6">
           <h2 className="text-gray-700 text-lg">Start a new document</h2>
@@ -19,7 +56,10 @@ const DocumentCreate = () => {
           </Button>
         </div>
         <div>
-          <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-300">
+          <div
+            onClick={() => setShowModal(true)}
+            className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-300"
+          >
             <Image
               src="https://ssl.gstatic.com/docs/templates/thumbnails/docs-blank-googlecolors.png"
               layout="fill"
