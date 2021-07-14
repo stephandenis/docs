bd3024a95a17df1195e964e44b1dd2ed8df86a9d
Stephan <stephangdenis@gmail.com>
2021-07-14 11:09:38 -0300

    fetch firestore doc - snapshots[DocumentRow]

--- components/DocumentHistory.js
+++ components/DocumentHistory.js
@@ -1,6 +1,19 @@
+import { useCollectionOnce } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import { useSession } from "next-auth/client";
 import Icon from "@material-tailwind/react/Icon";
+import DocumentRow from "./DocumentRow";
 
 const DocumentHistory = () => {
+  const [session] = useSession();
+  //fetchs the doc from firebase
+  const [snapshot] = useCollectionOnce(
+    db
+      .collection("userDocs")
+      .doc(session.user.email)
+      .collection("docs")
+      .orderBy("timestamp", "desc")
+  );
   return (
     <section className="bg-white px-10 md:px-0">
       <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
@@ -9,6 +22,15 @@ const DocumentHistory = () => {
           <p className="mr-12">Date Created</p>
           <Icon name="folder" size="3xl" color="gray" />
         </div>
+
+        {snapshot?.docs.map((doc) => (
+          <DocumentRow
+            key={doc.id}
+            id={doc.id}
+            fileName={doc.data().fileName}
+            date={doc.data().timestamp}
+          />
+        ))}
       </div>
     </section>
   );
